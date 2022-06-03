5862612eafbdd094005db7a079fc1b801299361f
prodip763 <prodip12763@gmail.com>
2022-06-04 05:09:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar1');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('navbar');
+    })
+}
